3f25d146de4da6f0e8c843c966518cabfc8f1ffc
SummaryPuppet <adrianm_sv@hotmail.com>
2024-12-29 20:46:07 -0500

    add max and min duration

--- mobile/components/PlayerProgressBar.jsx
+++ mobile/components/PlayerProgressBar.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { Text, View } from "react-native";
 import { Slider } from "react-native-awesome-slider";
 import { useSharedValue } from "react-native-reanimated";
 import TrackPlayer, { useProgress } from "react-native-track-player";
@@ -14,17 +15,29 @@ export default function PlayerProgressBar() {
     await TrackPlayer.seekTo(newPositionNumber * duration);
   };
 
+  const getMinutes = (time) => Math.floor(time / 60);
+  const getSeconds = (time) => Math.floor(time % 60);
+
   useEffect(() => {
     progress.value = duration > 0 ? position / duration : 0;
   });
 
   return (
-    <Slider
-      className="flex-none w-full"
-      progress={progress}
-      minimumValue={min}
-      maximumValue={max}
-      onSlidingComplete={(newPosition) => handleSliderChange(newPosition)}
-    />
+    <>
+      <Slider
+        className="flex-none w-full"
+        progress={progress}
+        minimumValue={min}
+        maximumValue={max}
+        onSlidingComplete={(newPosition) => handleSliderChange(newPosition)}
+      />
+
+      <View className="flex-row justify-between mt-4">
+        <Text className="text-white">0:00</Text>
+        <Text className="text-white">
+          {getMinutes(duration)}:{getSeconds(duration)}
+        </Text>
+      </View>
+    </>
   );
 }
