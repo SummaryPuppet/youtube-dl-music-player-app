7755f7d48fc0acc32eaeef277616003ef989edd5
SummaryPuppet <adrianm_sv@hotmail.com>
2024-12-29 21:46:37 -0500

    remove console.log

--- mobile/components/ui/ImagePlaceholder.jsx
+++ mobile/components/ui/ImagePlaceholder.jsx
@@ -3,7 +3,6 @@ import { useTheme } from "../../stores/theme";
 
 export default function ImagePlaceholder(props) {
   const { themes, currentTheme } = useTheme();
-  console.log(themes[currentTheme].primaryColor);
   return (
     <View
       className={`rounded-3xl h-72 w-72  self-center`}
