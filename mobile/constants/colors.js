f8c41b8f74de510562e70d50d432be9c3b0565af
SummaryPuppet <adrianm_sv@hotmail.com>
2024-12-29 21:26:46 -0500

    add color red

--- mobile/constants/colors.js
+++ mobile/constants/colors.js
@@ -3,16 +3,18 @@ export const theme = {
         primaryColor: "#3730a3",
         secondaryColor: "#312e81",
         bgPrimaryColor: "#111827",
-        bgTailwindColor: "indigo-700",
-        tailwindColor: "indigo",
         settingPlaceholdeColor: "#6366F1"
     },
     green: {
         primaryColor: "#047857",
         secondaryColor: "#065f46",
         bgPrimaryColor: "#011710",
-        bgTailwindColor: "emerald-700",
-        tailwindColor: "emerald",
         settingPlaceholdeColor: "#10B981"
     },
+    red: {
+        primaryColor: "#CC2B52",
+        secondaryColor: "#9D2C3C",
+        bgPrimaryColor: "#740938",
+        settingPlaceholdeColor: "#CC2B52"
+    }
 }
