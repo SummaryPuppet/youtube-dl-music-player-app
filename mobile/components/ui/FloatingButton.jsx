6a4eb788d23a7d0d98b08fc58b1a66ed48cbd6c1
SummaryPuppet <adrianm_sv@hotmail.com>
2025-01-05 00:26:51 -0500

    add ref

--- mobile/components/ui/FloatingButton.jsx
+++ mobile/components/ui/FloatingButton.jsx
@@ -1,21 +1,29 @@
+import { forwardRef } from "react";
 import { Pressable } from "react-native";
 import { useActiveTrack } from "react-native-track-player";
 import { useTheme } from "../../stores/theme";
 
-export default function FloatingButton({ onPress, children }) {
-  const { themes, currentTheme } = useTheme();
-  const track = useActiveTrack();
+const FloatingButton = forwardRef(
+  ({ onPress, moveForPlayer = true, children }, ref) => {
+    const { themes, currentTheme } = useTheme();
+    const track = useActiveTrack();
 
-  return (
-    <Pressable
-      style={{
-        backgroundColor: themes[currentTheme].primaryColor,
-        bottom: track ? 125 : 10,
-      }}
-      className="absolute z-50 p-4 rounded-full right-5"
-      onPress={onPress}
-    >
-      {children}
-    </Pressable>
-  );
-}
+    return (
+      <Pressable
+        ref={ref}
+        style={{
+          backgroundColor: themes[currentTheme].primaryColor,
+          bottom: moveForPlayer ? (track ? 125 : 60) : 40,
+        }}
+        className="absolute z-50 p-4 rounded-full right-5"
+        onPress={onPress}
+      >
+        {children}
+      </Pressable>
+    );
+  }
+);
+
+FloatingButton.displayName = "FloatingButton";
+
+export default FloatingButton;
