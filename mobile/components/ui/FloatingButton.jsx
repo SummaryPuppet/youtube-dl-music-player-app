8bf8da278800ca6981fd6ae56721ed61e72b4e09
SummaryPuppet <adrianm_sv@hotmail.com>
2025-01-04 23:03:17 -0500

    fix place

--- mobile/components/ui/FloatingButton.jsx
+++ mobile/components/ui/FloatingButton.jsx
@@ -1,15 +1,18 @@
 import { Pressable } from "react-native";
+import { useActiveTrack } from "react-native-track-player";
 import { useTheme } from "../../stores/theme";
 
 export default function FloatingButton({ onPress, children }) {
   const { themes, currentTheme } = useTheme();
+  const track = useActiveTrack();
 
   return (
     <Pressable
       style={{
         backgroundColor: themes[currentTheme].primaryColor,
+        bottom: track ? 125 : 10,
       }}
-      className="absolute z-50 p-4 rounded-full bottom-10 right-5"
+      className="absolute z-50 p-4 rounded-full right-5"
       onPress={onPress}
     >
       {children}
