9368ee06776c96dd5e3aeff8873732dfb022ed12
SummaryPuppet <adrianm_sv@hotmail.com>
2025-01-05 00:27:33 -0500

    add obj

--- mobile/storage/storage.js
+++ mobile/storage/storage.js
@@ -8,13 +8,33 @@ const set = async (key, value) => {
     }
 }
 
+const setObject = async (key, value) => {
+    try {
+        await AsyncStorage.setItem(key, JSON.stringify(value))
+    } catch (e) {
+        console.error(e)
+    }
+}
+
 const getString = async (key) => {
     const value = AsyncStorage.getItem(key)
 
     return value
 }
 
+const getObject = async (key) => {
+    const value = AsyncStorage.getItem(key)
+    try {
+        console.log(value)
+        return JSON.parse(value)
+    } catch (_) {
+        return {}
+    }
+}
+
 export const storage = {
     set,
-    getString
+    setObject,
+    getString,
+    getObject
 }
